01fe16c5b876e4bb4b7e0b9e0fefda99599cb1eb
anseki <AnSeki.aff@gmail.com>
2016-06-19 03:52:24 +0900

    Change anim.js getting rates

--- src/anim.js
+++ src/anim.js
@@ -1,5 +1,4 @@
 /* exported anim */
-/* global getPointOnCubic:false, getIntersections:false */
 
 var anim = (function() {
   'use strict';
@@ -9,6 +8,7 @@ var anim = (function() {
   /**
    * @callback callback
    * @param {number} rate - Progress [0, 1].
+   * @param {boolean|null} finish
    */
 
   /**
@@ -16,9 +16,9 @@ var anim = (function() {
    * @property {callback} callback
    * @property {number} duration
    * @property {number} count
-   * @property {number[]} timing - [x1, y1, x2, y2]
-   * @property {number} start
    * @property {boolean} isLinear
+   * @property {number[]} rates
+   * @property {number} start
    */
 
   // @EXPORT@
@@ -30,14 +30,14 @@ var anim = (function() {
       'ease-out': [0, 0, 0.58, 1],
       'ease-in-out': [0.42, 0, 0.58, 1]
     },
-    BASE_FPS: 60,
+    MSPF: 1000 / 60, // ms/frame (FPS: 60)
 
     tasks: [],
     request: window.requestAnimationFrame ||
       window.mozRequestAnimationFrame ||
       window.webkitRequestAnimationFrame ||
       window.msRequestAnimationFrame ||
-      function(callback) { setTimeout(callback, 1000 / anim.BASE_FPS); },
+      function(callback) { setTimeout(callback, anim.MSPF); },
     cancel: window.cancelAnimationFrame ||
       window.mozCancelAnimationFrame ||
       window.webkitCancelAnimationFrame ||
@@ -52,11 +52,45 @@ var anim = (function() {
      * @returns {number} - animID to remove.
      */
     add: function(callback, duration, count, timing) {
+      var isLinear = timing[0] === 0 && timing[1] === 0 && timing[2] === 1 && timing[3] === 1,
+        rates, stepX, stepT, nextX, t, point;
+
+      function getPoint(t) {
+        var t2 = t * t, t3 = t2 * t, t1 = 1 - t, t12 = t1 * t1,
+          p1f = 3 * t12 * t, p2f = 3 * t1 * t2;
+        return {
+          x: p1f * timing[0] + p2f * timing[2] + t3,
+          y: p1f * timing[1] + p2f * timing[3] + t3
+        };
+      }
+
       if (!anim.tasks.some(function(task) { return task.callback === callback; })) {
+
+        if (!isLinear) {
+          // Generate list
+          if (duration < anim.MSPF) {
+            rates = [0, 1];
+          } else {
+            stepX = anim.MSPF / duration;
+            stepT = stepX / 10; // precision
+            nextX = stepX;
+            rates = [0];
+            for (t = stepT; t <= 1; t += stepT) {
+              point = getPoint(t);
+              if (point.x >= nextX) {
+                rates.push(point.y);
+                nextX += stepX;
+              }
+            }
+            rates.push(1); // for tolerance
+          }
+        }
+
         anim.tasks.push({
-          callback: callback, duration: duration, count: count, timing: timing,
-          start: Date.now(),
-          isLinear: timing[0] === 0 && timing[1] === 0 && timing[2] === 1 && timing[3] === 1
+          callback: callback, duration: duration, count: count,
+          isLinear: isLinear,
+          rates: rates,
+          start: Date.now()
         });
       }
       anim.start();
@@ -76,13 +110,8 @@ var anim = (function() {
     },
     step: function() {
 
-      function getORatio(t, timing) {
-        var p1 = {x: timing[0], y: timing[1]}, p2 = {x: timing[2], y: timing[3]},
-          point = t === 0 ? {y: 0} : t >= 1 ? {y: 1} :
-            getPointOnCubic({x: 0, y: 0}, p1, p2, {x: 1, y: 1},
-              getIntersections({x: 0, y: 0}, p1, p2, {x: 1, y: 1},
-                {x: t, y: 0}, {x: t, y: 1})[0]);
-        return point.y;
+      function getORatio(rates, timeLen) {
+        return rates[Math.round(timeLen / anim.MSPF)];
       }
 
       var now = Date.now();
@@ -92,13 +121,13 @@ var anim = (function() {
         var timeLen = now - task.start, tRatio, loops;
 
         if (timeLen >= task.duration && task.count <= 1) {
-          task.callback(1);
+          task.callback(1, true);
           return false;
         }
         if (timeLen > task.duration) {
           loops = Math.floor(timeLen / task.duration);
           if (loops >= task.count) { // Must: task.count > 1
-            task.callback(1);
+            task.callback(1, true);
             return false;
           }
           task.count -= loops;
@@ -106,7 +135,14 @@ var anim = (function() {
           timeLen = now - task.start;
         }
         tRatio = timeLen / task.duration;
-        return task.callback(task.isLinear ? tRatio : getORatio(tRatio, task.timing)) !== false;
+        // return task.callback(task.isLinear ? tRatio : getORatio(tRatio, task.timing)) !== false;
+
+        // debug
+        window.testTimeS();
+        var oRatio = getORatio(task.rates, timeLen);
+        window.testTimeE();
+        return task.callback(oRatio, false, tRatio) !== false;
+        // /debug
       });
 
       if (anim.tasks.length) { anim.requestID = anim.request.call(window, anim.step); }
