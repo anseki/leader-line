cbe473fca9614ebd486d8d15ca44c092179bb062
anseki <AnSeki.aff@gmail.com>
2016-08-23 00:56:02 +0900

    viewBox_pathEdge -> viewBox_edge, Add events.cur_viewBox_edge

--- src/leader-line.js
+++ src/leader-line.js
@@ -155,7 +155,7 @@
       position_path: {}, position_lineStrokeWidth: {}, position_socketGravitySE: {hasSE: true},
       path_pathData: {},
       viewBox_bBox: {hasProps: true},
-      viewBox_plugBCircleSE: {hasSE: true}, viewBox_pathEdge: {hasProps: true},
+      viewBox_plugBCircleSE: {hasSE: true}, viewBox_edge: {hasProps: true},
       lineMask_enabled: {iniValue: false},
       lineMask_outlineMode: {iniValue: false},
       lineMask_x: {}, lineMask_y: {},
@@ -1821,20 +1821,39 @@
   function updatePath(props) {
     traceLog.add('<updatePath>'); // [DEBUG/]
     var curStats = props.curStats, aplStats = props.aplStats,
-      curPathData,
-      curPathEdge = curStats.viewBox_pathEdge,
+      curPathData, padding,
+      edge = {}, curEdge = curStats.viewBox_edge,
       pathList = props.pathList.animVal || props.pathList.baseVal,
       updated = false;
 
     if (pathList) {
-      curPathEdge.x1 = curPathEdge.x2 = pathList[0][0].x;
-      curPathEdge.y1 = curPathEdge.y2 = pathList[0][0].y;
+      edge.x1 = edge.x2 = pathList[0][0].x;
+      edge.y1 = edge.y2 = pathList[0][0].y;
       curStats.path_pathData = curPathData = pathList2PathData(pathList, function(point) {
-        if (point.x < curPathEdge.x1) { curPathEdge.x1 = point.x; }
-        if (point.x > curPathEdge.x2) { curPathEdge.x2 = point.x; }
-        if (point.y < curPathEdge.y1) { curPathEdge.y1 = point.y; }
-        if (point.y > curPathEdge.y2) { curPathEdge.y2 = point.y; }
+        if (point.x < edge.x1) { edge.x1 = point.x; }
+        if (point.x > edge.x2) { edge.x2 = point.x; }
+        if (point.y < edge.y1) { edge.y1 = point.y; }
+        if (point.y > edge.y2) { edge.y2 = point.y; }
       });
+      // Expand bBox with `line` or symbols
+      padding = Math.max(curStats.line_strokeWidth / 2,
+        curStats.viewBox_plugBCircleSE[0] || 0, curStats.viewBox_plugBCircleSE[1] || 0);
+      edge.x1 -= padding;
+      edge.y1 -= padding;
+      edge.x2 += padding;
+      edge.y2 += padding;
+
+      // Apply `viewBox_edge`
+      if (edge.x1 !== curEdge.x1 || edge.y1 !== curEdge.y1 ||
+          edge.x2 !== curEdge.x2 || edge.y2 !== curEdge.y2) {
+        traceLog.add('viewBox_edge'); // [DEBUG/]
+        curStats.viewBox_edge = edge;
+        updated = true;
+
+        if (props.events.cur_viewBox_edge) {
+          props.events.cur_viewBox_edge.forEach(function(handler) { handler(props, curStats.viewBox_edge); });
+        }
+      }
 
       // Apply `pathData`
       if (pathDataHasChanged(curPathData, aplStats.path_pathData)) {
@@ -1871,24 +1890,15 @@
   function updateViewBox(props) {
     traceLog.add('<updateViewBox>'); // [DEBUG/]
     var curStats = props.curStats, aplStats = props.aplStats,
-      curPathEdge = curStats.viewBox_pathEdge,
+      curEdge = curStats.viewBox_edge,
       curBBox = curStats.viewBox_bBox, aplBBox = aplStats.viewBox_bBox,
-      padding = Math.max(curStats.line_strokeWidth / 2,
-        curStats.viewBox_plugBCircleSE[0] || 0, curStats.viewBox_plugBCircleSE[1] || 0),
-      // Expand bBox with `line` or symbols
-      points = {
-        x1: curPathEdge.x1 - padding,
-        y1: curPathEdge.y1 - padding,
-        x2: curPathEdge.x2 + padding,
-        y2: curPathEdge.y2 + padding
-      },
       viewBox = props.svg.viewBox.baseVal, styles = props.svg.style,
       updated = false;
 
-    curBBox.x = curStats.lineMask_x = curStats.lineOutlineMask_x = curStats.maskBGRect_x = points.x1;
-    curBBox.y = curStats.lineMask_y = curStats.lineOutlineMask_y = curStats.maskBGRect_y = points.y1;
-    curBBox.width = points.x2 - points.x1;
-    curBBox.height = points.y2 - points.y1;
+    curBBox.x = curStats.lineMask_x = curStats.lineOutlineMask_x = curStats.maskBGRect_x = curEdge.x1;
+    curBBox.y = curStats.lineMask_y = curStats.lineOutlineMask_y = curStats.maskBGRect_y = curEdge.y1;
+    curBBox.width = curEdge.x2 - curEdge.x1;
+    curBBox.height = curEdge.y2 - curEdge.y1;
 
     ['x', 'y', 'width', 'height'].forEach(function(boxKey) {
       var value;
@@ -3919,6 +3929,30 @@
           traceLog.add('</ATTACHMENTS.caption.updateShow>'); // [DEBUG/]
         };
 
+        if (IS_WEBKIT) {
+          // [WEBKIT] overflow:visible is ignored
+          attachProps.adjustEdge = function(props, edge) {
+            traceLog.add('<ATTACHMENTS.caption.adjustEdge>'); // [DEBUG/]
+            var curStats = attachProps.curStats, textEdge, strokeWidth;
+            if (curStats.x != null) { // eslint-disable-line eqeqeq
+              textEdge = {x1: curStats.x, y1: curStats.y,
+                x2: curStats.x + attachProps.width, y2: curStats.y + attachProps.height};
+              if (attachProps.strokeWidth) {
+                strokeWidth = attachProps.strokeWidth / 2;
+                textEdge.x1 -= strokeWidth;
+                textEdge.y1 -= strokeWidth;
+                textEdge.x2 += strokeWidth;
+                textEdge.y2 += strokeWidth;
+              }
+              if (textEdge.x1 < edge.x1) { edge.x1 = textEdge.x1; }
+              if (textEdge.x2 > edge.x2) { edge.x2 = textEdge.x2; }
+              if (textEdge.y1 < edge.y1) { edge.y1 = textEdge.y1; }
+              if (textEdge.y2 > edge.y2) { edge.y2 = textEdge.y2; }
+            }
+            traceLog.add('</ATTACHMENTS.caption.adjustEdge>'); // [DEBUG/]
+          };
+        }
+
         traceLog.add('</ATTACHMENTS.caption.init>'); // [DEBUG/]
         return true;
       },
@@ -4042,6 +4076,7 @@
           text.styleStroke.strokeWidth = strokeWidth + 'px';
           text.styleStroke.stroke = attachProps.outlineColor;
         }
+        attachProps.strokeWidth = strokeWidth;
 
         if (!attachProps.color) { addEventHandler(props, 'cur_line_color', attachProps.updateColor); }
         if ((attachProps.refSocketXY =
@@ -4056,12 +4091,21 @@
           addEventHandler(props, 'apl_path', attachProps.updatePath);
         }
         addEventHandler(props, 'svgShow', attachProps.updateShow);
+        if (IS_WEBKIT) {
+          // [WEBKIT] overflow:visible is ignored
+          addEventHandler(props, 'cur_viewBox_edge', attachProps.adjustEdge);
+        }
         addDelayedProc(function() { // after updating `attachProps.boundTargets`
           attachProps.updateColor(props);
           if (attachProps.refSocketXY) {
             attachProps.updateSocketXY(props);
           } else {
             attachProps.updatePath(props);
+          }
+          if (IS_WEBKIT) {
+            // [WEBKIT] overflow:visible is ignored
+            attachProps.adjustEdge(props, props.curStats.viewBox_edge);
+            updateViewBox(props);
           }
           attachProps.updateShow(props);
         });
@@ -4083,6 +4127,13 @@
           removeEventHandler(props, 'apl_path', attachProps.updatePath);
         }
         removeEventHandler(props, 'svgShow', attachProps.updateShow);
+        if (IS_WEBKIT) {
+          // [WEBKIT] overflow:visible is ignored
+          removeEventHandler(props, 'cur_viewBox_edge', attachProps.adjustEdge);
+          addDelayedProc(function() { // reset viewBox_edge
+            if (updatePath(props)) { updateViewBox(props); }
+          });
+        }
 
         if (attachProps.elmsAppend) {
           attachProps.elmsAppend.forEach(function(elm) { props.svg.removeChild(elm); });
