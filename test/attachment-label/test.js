79c4218dab63ae34dee1c41e7cf58368b040f03c
anseki <AnSeki.aff@gmail.com>
2016-09-11 16:50:52 +0900

    Add attachments and paths to test/attachment-label

--- test/attachment-label/test.js
+++ test/attachment-label/test.js
@@ -9,12 +9,7 @@ window.addEventListener('load', function() {
     anchor1 = document.getElementById('anchor-1'),
     style = anchor1.style,
     ll = new LeaderLine(document.getElementById('anchor-0'), anchor1, {
-      color: 'rgba(255, 0, 0, 0.5)', endPlug: 'disc', endPlugSize: 4,
-      startLabel: '[startLabel]',
-      endLabel: '[endLabel]',
-      // startLabel: LeaderLine.captionLabel({text: '[startLabel]', offset: [0, 0]}),
-      // endLabel: LeaderLine.captionLabel({text: '[endLabel]', offset: [0, 0]}),
-      middleLabel: '[middleLabel]'
+      color: 'rgba(255, 0, 0, 0.5)', endPlug: 'disc', endPlugSize: 4
     }),
 
     body = document.body,
@@ -56,6 +51,45 @@ window.addEventListener('load', function() {
       ll.position();
     }
   }), false);
+
+  // switcher - label
+  (function() {
+    var select = document.getElementById('attachment-name');
+
+    function initLabel() {
+      switch (select.value) {
+        case 'captionLabel':
+          ll.setOptions({
+            startLabel: '[startLabel]',
+            endLabel: '[endLabel]',
+            // startLabel: LeaderLine.captionLabel({text: '[startLabel]', offset: [0, 0]}),
+            // endLabel: LeaderLine.captionLabel({text: '[endLabel]', offset: [0, 0]}),
+            middleLabel: '[middleLabel]'
+          });
+          break;
+
+        case 'pathLabel':
+          ll.setOptions({
+            startLabel: LeaderLine.pathLabel({text: '[startLabel]'}),
+            endLabel: LeaderLine.pathLabel({text: '[endLabel]'}),
+            middleLabel: LeaderLine.pathLabel({text: '[middleLabel]'})
+          });
+          break;
+        // no default
+      }
+    }
+
+    select.addEventListener('change', initLabel, false);
+    initLabel();
+  })();
+
+  // switcher - path
+  (function() {
+    var select = document.getElementById('path');
+    function initPath() { ll.path = select.value; }
+    select.addEventListener('change', initPath, false);
+    initPath();
+  })();
 
   window.ll = ll;
 }, false);
