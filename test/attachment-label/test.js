a4ad61861f4661c4aad9ac6efdea62d197d26a15
anseki <AnSeki.aff@gmail.com>
2016-10-19 21:05:23 +0900

    Update test/attachment-label (Draggable)

--- test/attachment-label/test.js
+++ test/attachment-label/test.js
@@ -1,57 +1,18 @@
-/* global LeaderLine:false, AnimEvent:false */
+/* global LeaderLine:false, Draggable:false */
 
 window.traceLog.enabled = true;
 window.addEventListener('load', function() {
   'use strict';
 
-  var CLASS_DRAGGING = 'dragging',
-
-    anchor1 = document.getElementById('anchor-1'),
-    style = anchor1.style,
+  var anchor1 = document.getElementById('anchor-1'),
     ll = new LeaderLine(document.getElementById('anchor-0'), anchor1, {
       color: 'rgba(255, 0, 0, 0.5)', endPlug: 'disc', endPlugSize: 4
-    }),
+    });
 
-    body = document.body,
-    moving = {element: anchor1, x: 0, y: 0},
-    activeMoving, offset;
-
-  anchor1.addEventListener('mousedown', function(e) {
-    var rectTarget = anchor1.getBoundingClientRect();
-    activeMoving = moving;
-    activeMoving.offset =
-      {x: rectTarget.left + window.pageXOffset - e.pageX, y: rectTarget.top + window.pageYOffset - e.pageY};
-    body.className = CLASS_DRAGGING;
-  }, false);
-
-  body.addEventListener('mouseup', function() {
-    activeMoving = null;
-    body.className = '';
-  }, false);
-
-  offset = (function() {
-    var rectView = document.getElementById('view').getBoundingClientRect(),
-      rectTarget = anchor1.getBoundingClientRect();
-    return {
-      x: rectView.left + window.pageXOffset,
-      y: rectView.top + window.pageYOffset,
-      width: rectView.width - rectTarget.width,
-      height: rectView.height - rectTarget.height
-    };
-  })();
-
-  body.addEventListener('mousemove', AnimEvent.add(function(e) {
-    if (activeMoving) {
-      activeMoving.x = e.pageX - offset.x + activeMoving.offset.x;
-      activeMoving.y = e.pageY - offset.y + activeMoving.offset.y;
-      activeMoving.x = activeMoving.x < 0 ? 0 : activeMoving.x > offset.width ? offset.width : activeMoving.x;
-      activeMoving.y = activeMoving.y < 0 ? 0 : activeMoving.y > offset.height ? offset.height : activeMoving.y;
-      style.left = activeMoving.x + 'px';
-      style.top = activeMoving.y + 'px';
-
-      ll.position();
-    }
-  }), false);
+  new Draggable(anchor1, { // eslint-disable-line no-new
+    onMove: function() { ll.position(); },
+    ignoreZIndex: true
+  });
 
   // switcher - label
   (function() {
