4758f8eeffe8403e9517e9242631383be681e5db
anseki <AnSeki.aff@gmail.com>
2018-05-22 17:50:28 +0900

    Fix: It is constructed again.

--- test/httpd.js
+++ test/httpd.js
@@ -13,8 +13,6 @@ const
     'plain-draggable'
   ],
 
-  http = require('http'),
-  staticAlias = require('node-static-alias'),
   logger = (() => {
     const log4js = require('log4js');
     log4js.configure({
@@ -30,42 +28,43 @@ const
       categories: {default: {appenders: ['out'], level: 'info'}}
     });
     return log4js.getLogger('node-static-alias');
-  })();
+  })(),
 
-http.createServer((request, response) => {
-  request.addListener('end', () => {
-    (new staticAlias.Server(DOC_ROOT, {
-      cache: false,
-      headers: {'Cache-Control': 'no-cache, must-revalidate'},
-      alias:
-        MODULE_PACKAGES.map(packageName =>
-          ({ // node_modules
-            match: new RegExp(`^/${packageName}/.+`),
-            serve: `${require.resolve(packageName).replace(
-              // Include `packageName` for nested `node_modules`
-              new RegExp(`^(.*[/\\\\]node_modules)[/\\\\]${packageName}[/\\\\].*$`), '$1')}<% reqPath %>`,
-            allowOutside: true
-          })
-        ).concat([
-          {
-            match: /^\/src/,
-            serve: '..<% reqPath %>',
-            allowOutside: true
-          }
-        ]),
-      logger
-    }))
-      .serve(request, response, e => {
-        if (e) {
-          response.writeHead(e.status, e.headers);
-          logger.error('(%s) %s', request.url, response.statusCode);
-          if (e.status === 404) {
-            response.end('Not Found');
-          }
-        } else {
-          logger.info('(%s) %s', request.url, response.statusCode);
+  staticAlias = new (require('node-static-alias')).Server(DOC_ROOT, {
+    cache: false,
+    headers: {'Cache-Control': 'no-cache, must-revalidate'},
+    alias:
+      MODULE_PACKAGES.map(packageName =>
+        ({ // node_modules
+          match: new RegExp(`^/${packageName}/.+`),
+          serve: `${require.resolve(packageName).replace(
+            // Include `packageName` for nested `node_modules`
+            new RegExp(`^(.*[/\\\\]node_modules)[/\\\\]${packageName}[/\\\\].*$`), '$1')}<% reqPath %>`,
+          allowOutside: true
+        })
+      ).concat([
+        {
+          match: /^\/src/,
+          serve: '..<% reqPath %>',
+          allowOutside: true
         }
-      });
+      ]),
+    logger
+  });
+
+require('http').createServer((request, response) => {
+  request.addListener('end', () => {
+    staticAlias.serve(request, response, error => {
+      if (error) {
+        response.writeHead(error.status, error.headers);
+        logger.error('(%s) %s', request.url, response.statusCode);
+        if (error.status === 404) {
+          response.end('Not Found');
+        }
+      } else {
+        logger.info('(%s) %s', request.url, response.statusCode);
+      }
+    });
   }).resume();
 }).listen(PORT);
 
